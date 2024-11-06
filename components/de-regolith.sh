f78ad608230c3a96f0a8ad40331d541a1ee4c536
Matteo Ferrini <mimmi.ferrini@gmail.com>
2024-11-06 21:09:26 +0100

    Remove package

--- components/de-regolith.sh
+++ components/de-regolith.sh
@@ -1,4 +1,4 @@
 wget -qO - https://regolith-desktop.org/regolith.key | gpg --dearmor | sudo tee /usr/share/keyrings/regolith-archive-keyring.gpg > /dev/null
 echo deb "[arch=amd64 signed-by=/usr/share/keyrings/regolith-archive-keyring.gpg] https://regolith-desktop.org/release-3_2-ubuntu-noble-amd64 noble main" | sudo tee /etc/apt/sources.list.d/regolith.list
 sudo apt update
-sudo apt install regolith-desktop regolith-session-flashback regolith-look-lascaille regolith-look-nord regolith-look-solarized-light regolith-look-ayu
+sudo apt install regolith-desktop regolith-session-flashback regolith-look-lascaille regolith-look-nord regolith-look-ayu
