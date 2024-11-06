77883300e65f1c556466947feb6db2ad0471a4c9
Matteo Ferrini <mimmi.ferrini@gmail.com>
2024-11-06 20:40:47 +0100

    Simplify and add regolith

--- install.sh
+++ install.sh
@@ -1,17 +1,16 @@
-# Exit immediately if a command exits with a non-zero status
 set -e
 
 gsettings set org.gnome.desktop.screensaver lock-enabled false
 gsettings set org.gnome.desktop.session idle-delay 0
 
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get install git
+
 echo "Installing desktop tools..."
 
-# Install desktop tools and tweaks
 for installer in ./components/*.sh; do source $installer; done
 
-# Revert to normal idle and lock settings
 gsettings set org.gnome.desktop.screensaver lock-enabled true
 gsettings set org.gnome.desktop.session idle-delay 300
 
-# Logout to pickup changes
-gum confirm "Ready to reboot for all settings to take effect?" && sudo reboot
+sudo reboot
