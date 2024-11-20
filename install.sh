bbfb1802f97c93d233af02f77d8fb2cbd77ddeb1
Matteo Ferrini <maf@lunar.app>
2024-11-20 23:29:17 +0100

    Remove issue

--- install.sh
+++ install.sh
@@ -12,14 +12,6 @@ echo "2. Installing apps..."
 
 for installer in ./2_apps/*.sh; do source $installer; done
 
-echo "3. Installing dotfiles..."
-
-for file in ./3_dotfiles/; do yes | cp -r $file ~/; done
-
-echo "4. Installing wallpapers..."
-
-for picture in ./4_wallpapers/*.png; do yes | cp $picture ~/Pictures/; done
-
 echo "Done!."
 
 sudo reboot
